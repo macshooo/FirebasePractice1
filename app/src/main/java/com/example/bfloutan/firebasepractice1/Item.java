d689a7e6572d5c053568f2f906aa2019a05599a8
Unknown <tan.louismarcel@gmail.com>
2017-11-17 14:08:06 +0800

    Initial Commit

---
+++ app/src/main/java/com/example/bfloutan/firebasepractice1/Item.java
@@ -0,0 +1,24 @@
+package com.example.bfloutan.firebasepractice1;
+
+/**
+ * Created by bfloutan on 17/11/2017.
+ */
+
+public class Item {
+
+    private String title;
+
+    public Item() {}
+
+    public Item(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
